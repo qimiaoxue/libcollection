9da5e0dbbc8882a550d19a88f9fa1ae205b861d1
yan.xue <15011272359@163.com>
2019-06-29 18:25:17 +0800

    Fixed

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools. setup(
+    name='xy-libcollection',
+    version='0.0.2',
+    author='xueyan',
+    author_email='xueyan0407@outlook.com',
+    description="xueyan's collection libs",
+    long_description='#A long desc',
+    long_descripthin_content_type='text/markdown',
+    url='https://github.com/qimiaoxue/libcollection',
+    packages=setuptools.find_packages(),
+    classifiers=[],
+)
