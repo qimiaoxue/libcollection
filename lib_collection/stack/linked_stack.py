af3e16e48d91873c60c00c2010a7ee945538662c
yan.xue <15011272359@163.com>
2019-07-18 14:22:45 +0800

    Add linked_stack

--- lib_collection/stack/linked_stack.py
+++ lib_collection/stack/linked_stack.py
@@ -1,5 +1,19 @@
-#from libcollection.lib_collection.node import Node
-from lib_collection.node import Node
+class Node(object):
+
+    def __init__(self, v):
+        self.v = v
+        self.next = None
+
+    def __repr__(self):
+        """
+        >>> n = Node('a')
+        >>> n
+        Node('a')
+        >>> n = Node(1)
+        >>> n
+        Node(1)
+        """
+        return 'Node({})'.format(repr(self.v))
 
 
 class LinkedStack(object):
@@ -11,18 +25,54 @@ class LinkedStack(object):
     def __len__(self):
         return self.n
 
-    def __str__(self):
-        pass
+    def __repr__(self):
+        """
+        >>> stack = LinkedStack()
+        >>> stack.push('a')
+        >>> stack.push('b')
+        >>> stack.push('c')
+        >>> stack
+        LinkedStack(['c', 'b', 'a'])
+        """
+        return 'LinkedStack([{}])'.format(', '.join(repr(i) for i in self))
 
-    def __contains__(self):
-        pass
+    def __contains__(self, item):
+        """
+        >>> stack = LinkedStack()
+        >>> stack.push('a')
+        >>> stack.push('b')
+        >>> 'a' in stack
+        True
+        >>> 'b' in stack
+        True
+        >>> 'c' in stack
+        False
+        """
+        for i in self:
+            if i == item:
+                return True
+        return False
 
     def __iter__(self):
-        pass
+        """
+        >>> stack = LinkedStack()
+        >>> stack.push('a')
+        >>> stack.push('b')
+        >>> stack.push('c')
+        >>> for i in stack:
+        ...     print(i)
+        c
+        b
+        a
+        """
+        n = self.head.next
+        while n:
+            yield n.v
+            n = n.next
 
     def push(self, i):
         """
-        >>> stack = LikedStack()
+        >>> stack = LinkedStack()
         >>> stack.push('a')
         >>> len(stack)
         1
@@ -36,10 +86,39 @@ class LinkedStack(object):
         self.n += 1
 
     def pop(self):
-        pass
-
+        """
+        >>> stack = LinkedStack()
+        >>> stack.push('a')
+        >>> stack.push('b')
+        >>> len(stack)
+        2
+        >>> stack.pop()
+        'b'
+        >>> len(stack)
+        1
+        >>> stack.pop()
+        'a'
+        >>> len(stack)
+        0
+        """
+        if len(self) == 0:
+            raise IndexError('pop from empty stack')
+        n = self.head.next
+        self.head.next = n.next
+        self.n -= 1
+        return n.v
+    
+    @property
     def top(self):
-        pass
+        """
+        >>> stack = LinkedStack()
+        >>> stack.push('a')
+        >>> stack.top
+        'a'
+        """
+        if len(self) == 0:
+            raise IndexError('pop from empty stack')
+        return self.head.next.v
 
 
 if __name__ == '__main__':
